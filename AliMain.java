1c1d859a38fc48459d1683125867778301c6b94d
alisaidas <91589092+alisaidas@users.noreply.github.com>
2021-10-12 16:18:21 +0700

    Add files via upload

---
+++ AliMain.java
@@ -0,0 +1,31 @@
+public class Main {
+  public static void main(String[] args) {
+  	// Membuat nama alamat dari peserta
+    // first name , lastname, age, phone number, address, and other
+    //System.out.println("Hello World");
+    
+    // Inisialisasi
+    String first_name = "";
+    String last_name = "";
+    long phone_number;
+    String address = "";
+    int age = 0;
+    
+    // Input variabel
+    first_name = "Ali";
+    last_name = "Mubarak";
+    phone_number = 1234559;
+    address = "Palembang";
+    age = 21;
+    
+    
+    // Print in to screen
+    System.out.println("=======================================");
+    System.out.println("||      First name   = " + first_name );
+    System.out.println("||      Last name    = " + last_name);
+    System.out.println("||      Phone number = " + phone_number);
+    System.out.println("||      Address      = " + address);
+    System.out.println("||      Age          = " + age);
+    System.out.println("=======================================");
+  }
+}
